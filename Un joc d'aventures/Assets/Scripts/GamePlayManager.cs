bf16b4051db19bab70bd09911d13f5da8956cbf1
alex <alexd.aluma@gmail.com>
2018-09-20 20:35:23 +0200

    GamePlayerManager not finished

--- Un joc d'aventures/Assets/Scripts/GamePlayManager.cs
+++ Un joc d'aventures/Assets/Scripts/GamePlayManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GamePlayManager : MonoBehaviour
 {
@@ -14,5 +15,72 @@ public class GamePlayManager : MonoBehaviour
         public NodeVisited nodeVisited;
     }
 
+    public Text historyText;
+    public Transform answerParent;
+    public GameObject buttonAnswer;
     private StoryNode current;
+
+    private void Start()
+    {
+        current = StoryFiller.FillStory();
+        historyText.text = string.Empty;
+        FillUI();
+    }
+
+    void answerSelected(int index)
+    {
+        print(index);
+        historyText.text += "\n" + current.answers[index];
+
+        if (!current.isFinal)
+        {
+            current = current.nextNode[index];
+            FillUI();
+        }
+        else
+        {
+            //final del juego
+        }
+    }
+
+    void FillUI()
+    {
+        historyText.text += "\n" + current.history;
+
+        foreach (Transform child in answerParent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        bool isLeft = true;
+        float height = 50;
+        int index = 0;
+
+        foreach (string answer in current.answers)
+        {
+            GameObject buttonAnswerCopy = Instantiate(buttonAnswer);
+            buttonAnswerCopy.transform.parent = answerParent;
+            float x = buttonAnswerCopy.GetComponent<RectTransform>().rect.x * 1.1f;
+            buttonAnswerCopy.GetComponent<RectTransform>().localPosition = new Vector3(isLeft ? x : -x, height, 0);
+
+            if (!isLeft)
+            {
+                height += buttonAnswerCopy.GetComponent<RectTransform>().rect.y * 2.5f;
+
+                isLeft = !isLeft;
+                FillListener(buttonAnswerCopy.GetComponent<Button>(), index);
+                buttonAnswerCopy.GetComponentInChildren<Text>().text = answer;
+
+                index++;
+            }
+        }
+    }
+
+    void FillListener(Button button, int index)
+    {
+        button.onClick.AddListener(() =>
+        {
+            answerSelected(index);
+        });
+    }
 }
--- Un joc d'aventures/Assets/Scripts/StoryFiller.cs
+++ Un joc d'aventures/Assets/Scripts/StoryFiller.cs
@@ -27,7 +27,53 @@ public class StoryFiller : MonoBehaviour
         node1.nextNode[0] = node2;
         node2.nextNode[0] = node1;
 
+        GamePlayManager.StoryNode node3 = CreateNode("Parece que habla de plantas, qué sorpresa. Hay una señalada, se llama plantus corrientis", new string[] { "Dejar el libro en su sitio y explorar el resto de objetos de la habitación" });
+        node3.nextNode[0] = node1;
+        node2.nextNode[1] = node3;
 
+        GamePlayManager.StoryNode node4 = CreateNode("Nada interesante en las cajas, estan llenas de libros... deberían estar en la estantería", new string[] { "Volver y explorar el resto de objetos de la habitación" });
+        node1.nextNode[2] = node4;
+        node4.nextNode[0] = node1;
+
+        GamePlayManager.StoryNode node5 = CreateNode("Humm, una silla. Te duele la cabeza, así que te sientas", new string[] { "Eso está muy bien, pero yo quiero ver lo de la mesa también" });
+        node1.nextNode[1] = node5;
+
+        GamePlayManager.StoryNode node6 = CreateNode("La mesa en sí no tiene nada de especial, tiene un poco de tierra de la planta. Los cajones de la mesa parecen entreabiertos", new string[] { "Explorar los cajones", "Volver a explorar los otros objetos" });
+        GamePlayManager.StoryNode node6bis = CreateNode("La mesa en sí no tiene nada de especial, tiene un poco de tierra de la planta. La etiqueta de la planta pone plantus corrientis. Los cajones de la mesa parecen entreabiertos", new string[] { "Explorar los cajones", "Mirar la planta de cerca", "Volver a explorar los otros objetos" });
+
+        node5.nextNode[0] = node6;
+        node3.nodeVisited = () =>
+        {
+            node5.nextNode[0] = node6bis;
+        };
+        node6bis.nextNode[2] = node6.nextNode[1] = node1;
+
+        GamePlayManager.StoryNode node7 = CreateNode("Los cajones estan vacíos, mejor explorar otra cosa", new string[] { "Volver a explorar los otros objetos" });
+        node6.nextNode[0] = node6bis.nextNode[0] = node7;
+        node7.nextNode[0] = node1;
+
+        GamePlayManager.StoryNode node8 = CreateNode("¡¡Al levantar la planta te encuentras una llave!! ¿Qué abrirá?", new string[] { "Explorar la habitación" });
+        node6bis.nextNode[1] = node8;
+
+        GamePlayManager.StoryNode node9 = CreateNode("La habitación tiene un par de ventanas y una puerta", new string[] { "Ir a la ventana #1", "Ir a la ventana #2", "Ir a la puerta" });
+        root.nextNode[1] = node1.nextNode[0] = node8.nextNode[0] = node9;
+
+        GamePlayManager.StoryNode node10 = CreateNode("La ventana está tapiada, no se puede abrir", new string[] { "Ir a la otra ventana", "Ir a la puerta" });
+        node9.nextNode[0] = node9.nextNode[1] = node10;
+        node10.nextNode[0] = node10;
+
+        GamePlayManager.StoryNode node11 = CreateNode("La puerta está cerrada con un candado", new string[] { "Explorar los objetos de la habitación" });
+        GamePlayManager.StoryNode node11bis = CreateNode("La puerta está cerrada con un candado", new string[] { "Explorar los objetos de la habitación", "Usar la llave" });
+        node11.nextNode[0] = node11bis.nextNode[0] = node1;
+        node9.nextNode[2] = node10.nextNode[1] = node11;
+        node8.nodeVisited = () =>
+        {
+            node9.nextNode[2] = node10.nextNode[1] = node11bis;
+        };
+
+        GamePlayManager.StoryNode node12 = CreateNode("¡¡Has salido de la habitación!!", new string[] { "Salir del juego" });
+        node11bis.nextNode[1] = node12;
+        node12.isFinal = true;
 
         return root;
     }
